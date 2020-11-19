fdbac72dcbe8c78ded51f78976f3e2c7bad1fbdb
Marek Kukulski <marekkukulskip@gmail.com>
2020-11-19 22:17:39 +0100

    user.php

---
+++ user.php
@@ -0,0 +1,38 @@
+<?php
+	require_once 'database.php';
+
+	class User{//dane pobranego usera api
+
+     function getData(){//polaczenie z api + pobranie danych do zmienne $this->decoded
+        $url = "https://jsonplaceholder.typicode.com/users/1";
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+        $resp = curl_exec($ch);
+
+        if($e = curl_error($ch)){
+            echo $e;
+        }else{
+            $this->decoded = json_decode($resp);
+        }
+        curl_close($ch);
+    }
+
+   function getDomain(){//ucinanie po malpie
+		return explode('@', $this->decoded->email)[1];
+    }
+
+    function getPersonData(){//wyswietlenie w formacie JSON
+        return json_encode($this->decoded, JSON_PRETTY_PRINT);
+    }
+  }
+    $db = new Database;
+    $db->create();
+    $db->add();
+   
+    $user = new User();
+	$user->getData();
+
+
